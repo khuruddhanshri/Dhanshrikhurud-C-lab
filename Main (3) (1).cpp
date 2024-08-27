e4a402b2d2b260294196f4ed4d6f3eb4c9324723
khuruddhanshri <dhanashri.khurud24@sanjivani.edu.in>
2024-08-27 16:19:32 +0530

    Add files via upload

---
+++ Main (3) (1).cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int number=178;
+    //check if the number is greater than 5
+    if (number>5)
+    {
+        cout<<"the number is greater than 5"<<endl;
+    }else{
+        cout <<"the number is 5 or less"<<endl;
+    }
+    return 0;
+}
